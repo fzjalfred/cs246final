27fa120b9680602e468f40f38c0d05127b764f42
fzjalfred <fzjalfred@gmail.com>
2020-12-12 12:04:54 +0800

    Board board(); fixed

--- constructor/main.cc
+++ constructor/main.cc
@@ -228,7 +228,7 @@ int main(int argc, char* argv[]) {
 
     cin.exceptions(ios::eofbit|ios::failbit);
     string cmd;
-    Board board();
+    Board board;
     try {
         board.init(curTurn, curData, layout, geese);
     } catch (exception& e) {
